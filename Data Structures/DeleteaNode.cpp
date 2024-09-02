de0f6728bf11db1b8e5c713857b50b3f617bd756
Hiruna De Silva <139198764+HirunaD@users.noreply.github.com>
2024-09-02 17:10:21 +0530

    13th commit

---
+++ Data Structures/DeleteaNode.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+    // If the list is empty, return null
+    if (llist == nullptr) {
+        return nullptr;
+    }
+
+    // If the node to be deleted is the head node
+    if (position == 0) {
+        SinglyLinkedListNode* newHead = llist->next;
+        delete llist; // Free the memory of the old head
+        return newHead; // Return the new head
+    }
+
+    // Traverse the list to find the node just before the one we want to delete
+    SinglyLinkedListNode* current = llist;
+    for (int i = 0; i < position - 1 && current != nullptr; i++) {
+        current = current->next;
+    }
+
+    // If the node to be deleted is not null
+    if (current != nullptr && current->next != nullptr) {
+        SinglyLinkedListNode* nodeToDelete = current->next;
+        current->next = current->next->next; // Bypass the node to be deleted
+        delete nodeToDelete; // Free the memory of the deleted node
+    }
+
+    // Return the head of the list
+    return llist;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist1 = deleteNode(llist->head, position);
+
+    print_singly_linked_list(llist1, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist1);
+
+    fout.close();
+
+    return 0;
+}
